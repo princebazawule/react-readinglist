75d490fc3e07b16afe05d4dd093a60ca51155ae3
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-25 17:11:25 -0400

    feat: update app title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <h1>Reading List</h1>
+            <h1>The Reading List</h1>
             <p>Currently you have { books.length } books to get through...</p>
         </div>
     )
